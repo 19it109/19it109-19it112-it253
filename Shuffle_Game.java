a37c15c041e31d4e1709df8dc58a309fe72a215a
19it109 <68846796+19it109@users.noreply.github.com>
2020-11-05 21:55:33 +0530

    Add files via upload

--- Shuffle_Game.java
+++ Shuffle_Game.java
@@ -1,12 +1,14 @@
-
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.FontMetrics;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.util.Random;
@@ -14,290 +16,320 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
+import javax.swing.Timer;
 
 public class Shuffle_Game extends JPanel { // our grid will be drawn in a dedicated Panel
 
-    // Size of our Game instance
-    private int size;
-    // Number of tiles
-    private int nbTiles;
-    // Grid UI Dimension
-    private int dimension;
-    // Foreground Color
-    private static Color FOREGROUND_COLOR; // we use arbitrary color
-    // Random object to shuffle tiles
-    private static final Random RANDOM = new Random();
-    // Storing the tiles in a 1D Array of integers
-    private int[] tiles;
-    // Size of tile on UI
-    private int tileSize;
-    // Position of the blank tile
-    private int blankPos;
-    // Margin for the grid on the frame
-    private int margin;
-    // Grid UI Size
-    private int gridSize;
-    public static int n = 2;
-    private boolean gameOver;
-    // true if game over, false otherwise
-    public int cnt = 1;
-    static JFrame frame = new JFrame();
-    JFrame f1 = new JFrame();
-   // JTextField l = new JTextField(20);
-    JLabel moves = new JLabel("Moves :");
-    JLabel timer = new JLabel("Timeer : ");
+	// Size of our Game instance
+	private int size;
+	// Number of tiles
+	private int nbTiles;
+	// Grid UI Dimension
+	private int dimension;
+	// Foreground Color
+	private static Color FOREGROUND_COLOR; // we use arbitrary color
+	// Random object to shuffle tiles
+	private static final Random RANDOM = new Random();
+	// Storing the tiles in a 1D Array of integers
+	private int[] tiles;
+	// Size of tile on UI
+	private int tileSize;
+	// Position of the blank tile
+	private int blankPos;
+	// Margin for the grid on the frame
+	private int margin;
+	// Grid UI Size
+	private int gridSize;
+	public static int n = 2;
+	public int t = 1;
+	private boolean gameOver;
+	// true if game over, false otherwise
+	public int cnt = 1;
+	static JFrame frame = new JFrame();
+	static JPanel p = new JPanel();
+	JLabel moves = new JLabel("Moves");
+	JLabel label = new JLabel("count");
+	JLabel t1 = new JLabel("00" + "00" + "00");
+	int sec = 0;
+	int min = 0;
+	int hr = 0;
+	
 
-    public Shuffle_Game(int size, int dim, int mar) {
-        this.size = size;
-        dimension = dim;
-        margin = mar;
+	public Shuffle_Game(int size, int dim, int mar) {
+		this.size = size;
+		dimension = dim;
+		margin = mar;
 
-        // init tiles
-        nbTiles = size * size - 1; // -1 because we don't count blank tile
-        tiles = new int[size * size];
+		// init tiles
+		nbTiles = size * size - 1; // -1 because we don't count blank tile
+		tiles = new int[size * size];
 
-        // calculate grid size and tile size
-        gridSize = (dim - 2 * margin);
-        tileSize = gridSize / size;
+		// calculate grid size and tile size
+		gridSize = (dim - 2 * margin);
+		tileSize = gridSize / size;
 
-        setPreferredSize(new Dimension(dimension, dimension + margin));
-        setBackground(Color.WHITE);
-        setForeground(FOREGROUND_COLOR);
-        setFont(new Font("SansSerif", Font.BOLD, 60));
+		setPreferredSize(new Dimension(dimension, dimension + margin));
+		setBackground(Color.WHITE);
+		setForeground(FOREGROUND_COLOR);
+		setFont(new Font("SansSerif", Font.BOLD, 60));
 
-        gameOver = true;
+		gameOver = true;
 
-        addMouseListener(new MouseAdapter() {
-            @Override
-            public void mousePressed(MouseEvent e) {
-                // used to let users to interact on the grid by clicking
-                if (gameOver) {
-                    newGame();
-                } else {
-                    // get position of the click
-                    int ex = e.getX() - margin;
+		addMouseListener(new MouseAdapter() {
+			@Override
+			public void mousePressed(MouseEvent e) {
+				// used to let users to interact on the grid by clicking
+				if (gameOver) {
+					newGame();
+				} else {
+					// get position of the click
+					int ex = e.getX() - margin;
 
-                    int ey = e.getY() - margin;
+					int ey = e.getY() - margin;
 
-                    // click in the grid ?
-                    if (ex < 0 || ex > gridSize || ey < 0 || ey > gridSize) {
-                        return;
-                    }
+					// click in the grid ?
+					if (ex < 0 || ex > gridSize || ey < 0 || ey > gridSize) {
+						return;
+					}
 
-                    // get position in the grid
-                    int c1 = ex / tileSize;
-                    int r1 = ey / tileSize;
+					// get position in the grid
+					int c1 = ex / tileSize;
+					int r1 = ey / tileSize;
 
-                    // get position of the blank cell
-                    int c2 = blankPos % size;
-                    int r2 = blankPos / size;
+					// get position of the blank cell
+					int c2 = blankPos % size;
+					int r2 = blankPos / size;
 
-                    // we convert in the 1D coord
-                    int clickPos = r1 * size + c1;
+					// we convert in the 1D coord
+					int clickPos = r1 * size + c1;
 
-                    int dir = 0;
+					int dir = 0;
 
-                    // we search direction for multiple tile moves at once
-                    if (c1 == c2 && Math.abs(r1 - r2) > 0) {
-                        dir = (r1 - r2) > 0 ? size : -size;
-                    } else if (r1 == r2 && Math.abs(c1 - c2) > 0) {
-                        dir = (c1 - c2) > 0 ? 1 : -1;
-                    }
+					// we search direction for multiple tile moves at once
+					if (c1 == c2 && Math.abs(r1 - r2) > 0) {
+						dir = (r1 - r2) > 0 ? size : -size;
+					} else if (r1 == r2 && Math.abs(c1 - c2) > 0) {
+						dir = (c1 - c2) > 0 ? 1 : -1;
+					}
 
-                    if (dir != 0) {
-                        // we move tiles in the direction
-                        do {
-                            int newBlankPos = blankPos + dir;
-                            tiles[blankPos] = tiles[newBlankPos];
-                            blankPos = newBlankPos;
-                        } while (blankPos != clickPos);
+					if (dir != 0) {
+						// we move tiles in the direction
+						do {
+							int newBlankPos = blankPos + dir;
+							tiles[blankPos] = tiles[newBlankPos];
+							blankPos = newBlankPos;
+						} while (blankPos != clickPos);
 
-                        tiles[blankPos] = 0;
-                    }
+						tiles[blankPos] = 0;
+					}
 
-                    // we check if game is solved
-                    gameOver = isSolved();
+					// we check if game is solved
+					gameOver = isSolved();
+					if (dir == 1 || dir == -1 || dir == n || dir == -n) {
+						String d = Integer.toString(cnt++);
+						moves.setFont(new Font("Arial", Font.BOLD, 20));
+						// p.setLayout(new FlowLayout());
+						moves.setText("Moves: " + d);
+						add(moves);
+					}
 
-                    if (dir == 1 || dir == -1 || dir == n || dir == -n) {
-                        String d = Integer.toString(cnt++);
-                        moves.setText("Moves: " + d);
-                        add(moves);
-                    }
+				}
 
-                }
+				// we repaint panel
+				repaint();
+			}
+		});
+		newGame();
+		timer();
+	}
 
-                // we repaint panel
-                repaint();
-            }
-        });
-        newGame();
-    }
+	public void timer() {
 
-    private void newGame() {
-        do {
+		Timer time = new Timer(1000, new ActionListener() {
 
-            reset(); // reset in intial state
-            shuffle(); // shuffle
-        } while (!isSolvable()); // make it until grid be solvable
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				// TODO Auto-generated method stub
+				sec++;
+				if (sec >= 60) {
+					min++;
+					sec = 0;
+				} else if (min >= 60) {
+					hr++;
+					min = 0;
+					sec = 0;
+				}
+				t1.setFont(new Font("", Font.BOLD, 20));
+				t1.setText("Time " + hr + ":" + min + ":" + sec);
+				add(t1, new FlowLayout().LEFT);
+			}
+		});
+		time.start();
+	}
 
-        gameOver = false;
-    }
+	private void newGame() {
+		do {
+			reset(); // reset in intial state
+			shuffle(); // shuffle
+		} while (!isSolvable()); // make it until grid be solvable
 
-    private void reset() {
-        for (int i = 0; i < tiles.length; i++) {
-            tiles[i] = (i + 1) % tiles.length;
-        }
+		gameOver = false;
+	}
 
-        // we set blank cell at the last
-        blankPos = tiles.length - 1;
-    }
+	private void reset() {
+		for (int i = 0; i < tiles.length; i++) {
+			tiles[i] = (i + 1) % tiles.length;
+		}
 
-    private void shuffle() {
-        // don't include the blank tile in the shuffle, leave in the solved position
-        int n = nbTiles;
+		// we set blank cell at the last
+		blankPos = tiles.length - 1;
+	}
 
-        while (n > 1) {
-            int r = RANDOM.nextInt(n--);
-            int tmp = tiles[r];
-            tiles[r] = tiles[n];
-            tiles[n] = tmp;
-        }
-    }
+	private void shuffle() {
+		// don't include the blank tile in the shuffle, leave in the solved position
+		int n = nbTiles;
 
-    // Only half permutations of the puzzle are solvable
-    // Whenever a tile is preceded by a tile with higher value it counts
-    // as an inversion. In our case, with the blank tile in the solved position,
-    // the number of inversions must be even for the puzzle to be solvable
-    private boolean isSolvable() {
-        int countInversions = 0;
+		while (n > 1) {
+			int r = RANDOM.nextInt(n--);
+			int tmp = tiles[r];
+			tiles[r] = tiles[n];
+			tiles[n] = tmp;
+		}
+	}
 
-        for (int i = 0; i < nbTiles; i++) {
-            for (int j = 0; j < i; j++) {
-                if (tiles[j] > tiles[i]) {
-                    countInversions++;
-                    System.out.println(countInversions);
-                }
-            }
-        }
+	// Only half permutations of the puzzle are solvable
+	// Whenever a tile is preceded by a tile with higher value it counts
+	// as an inversion. In our case, with the blank tile in the solved position,
+	// the number of inversions must be even for the puzzle to be solvable
+	private boolean isSolvable() {
+		int countInversions = 0;
 
-        return countInversions % 2 == 0;
-    }
+		for (int i = 0; i < nbTiles; i++) {
+			for (int j = 0; j < i; j++) {
+				if (tiles[j] > tiles[i]) {
+					countInversions++;
+					System.out.println(countInversions);
+				}
+			}
+		}
 
-    private boolean isSolved() {
-        if (tiles[tiles.length - 1] != 0) // if blank tile is not in the solved position ==> not solved
-        {
-            return false;
-        }
+		return countInversions % 2 == 0;
+	}
 
-        for (int i = nbTiles - 1; i >= 0; i--) {
-            if (tiles[i] != i + 1) {
-                return false;
-            }
-        }
+	private boolean isSolved() {
+		if (tiles[tiles.length - 1] != 0) // if blank tile is not in the solved position ==> not solved
+		{
+			return false;
+		}
 
-        return true;
-    }
+		for (int i = nbTiles - 1; i >= 0; i--) {
+			if (tiles[i] != i + 1) {
+				return false;
+			}
+		}
 
-    private void drawGrid(Graphics2D g) {
-        for (int i = 0; i < tiles.length; i++) {
-            // we convert 1D coords to 2D coords given the size of the 2D Array
-            int r = i / size;
-            int c = i % size;
-            // we convert in coords on the UI
-            int x = margin + c * tileSize;
-            int y = margin + r * tileSize;
+		return true;
+	}
 
-            // check special case for blank tile
-            if (tiles[i] == 0) {
-                if (gameOver) {
-                    g.setColor(FOREGROUND_COLOR);
-                    drawCenteredString(g, "\u2713", x, y);
-                }
+	private void drawGrid(Graphics2D g) {
+		for (int i = 0; i < tiles.length; i++) {
+			// we convert 1D coords to 2D coords given the size of the 2D Array
+			int r = i / size;
+			int c = i % size;
+			// we convert in coords on the UI
+			int x = margin + c * tileSize;
+			int y = margin + r * tileSize;
 
-                continue;
-            }
+			// check special case for blank tile
+			if (tiles[i] == 0) {
+				if (gameOver) {
+					g.setColor(FOREGROUND_COLOR);
+					drawCenteredString(g, "\u2713", x, y);
+				}
 
-            // for other tiles
-            g.setColor(getForeground());
-            g.fillRoundRect(x, y, tileSize, tileSize, 25, 25);
-            g.setColor(Color.WHITE);
-            g.drawRoundRect(x, y, tileSize, tileSize, 25, 25);
-            g.setColor(Color.WHITE);
+				continue;
+			}
 
-            drawCenteredString(g, String.valueOf(tiles[i]), x, y);
-        }
-    }
+			// for other tiles
+			g.setColor(getForeground());
+			g.fillRoundRect(x, y, tileSize, tileSize, 25, 25);
+			g.setColor(Color.WHITE);
+			g.drawRoundRect(x, y, tileSize, tileSize, 25, 25);
+			g.setColor(Color.WHITE);
 
-    private void drawStartMessage(Graphics2D g) {
-        if (gameOver) {
-            g.setFont(getFont().deriveFont(Font.BOLD, 18));
-            g.setColor(FOREGROUND_COLOR);
-            String s = "Click to start new game";
-            g.drawString(s, (getWidth() - g.getFontMetrics().stringWidth(s)) / 2, getHeight() - margin);
-            setVisible(false);
-            f1.setVisible(false);
-            nextGame();
+			drawCenteredString(g, String.valueOf(tiles[i]), x, y);
+		}
+	}
 
-        }
-    }
+	private void drawStartMessage(Graphics2D g) {
+		if (gameOver) {
+			g.setFont(getFont().deriveFont(Font.BOLD, 18));
+			g.setColor(FOREGROUND_COLOR);
+			String s = "Click to start new game";
+			g.drawString(s, (getWidth() - g.getFontMetrics().stringWidth(s)) / 2, getHeight() - margin);
 
-    private void drawCenteredString(Graphics2D g, String s, int x, int y) {
-        // center string s for the given tile (x,y)
-        FontMetrics fm = g.getFontMetrics();
-        int asc = fm.getAscent();
-        int desc = fm.getDescent();
-        g.drawString(s, x + (tileSize - fm.stringWidth(s)) / 2, y + (asc + (tileSize - (asc + desc)) / 2));
-    }
+			setVisible(false);
+			nextGame();
 
-    @Override
-    protected void paintComponent(Graphics g) {
-        super.paintComponent(g);
-        Graphics2D g2D = (Graphics2D) g;
-        g2D.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-        drawGrid(g2D);
-        drawStartMessage(g2D);
-    }
+		}
+	}
 
-    public static void nextGame() {
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setTitle("Shuffle Game");
-        frame.setResizable(false);
+	private void drawCenteredString(Graphics2D g, String s, int x, int y) {
+		// center string s for the given tile (x,y)
+		FontMetrics fm = g.getFontMetrics();
+		int asc = fm.getAscent();
+		int desc = fm.getDescent();
+		g.drawString(s, x + (tileSize - fm.stringWidth(s)) / 2, y + (asc + (tileSize - (asc + desc)) / 2));
+	}
 
-        n++;
-        if (n == 3) {
+	@Override
+	protected void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		Graphics2D g2D = (Graphics2D) g;
+		g2D.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+		drawGrid(g2D);
+		drawStartMessage(g2D);
+	}
 
-            FOREGROUND_COLOR = new Color(255, 204, 51);
-        } else if (n == 4) {
-            FOREGROUND_COLOR = new Color(51, 153, 255);
-        } else if (n == 5) {
-            FOREGROUND_COLOR = new Color(0, 255, 51);
-        } else if (n == 6) {
-            FOREGROUND_COLOR = new Color(255, 153, 0);
-        } else if (n == 7) {
-            FOREGROUND_COLOR = new Color(102, 0, 153);
-        } else if (n == 8) {
-            FOREGROUND_COLOR = new Color(255, 0, 0);
-        }
+	public static void nextGame() {
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setTitle("Shuffle Game");
+		frame.setResizable(false);
 
-        frame.add(new Shuffle_Game(n, 800, 100), BorderLayout.CENTER);
+		n++;
+		if (n == 3) {
 
-        frame.pack();
+			FOREGROUND_COLOR = new Color(255, 204, 51);
+		} else if (n == 4) {
+			FOREGROUND_COLOR = new Color(51, 153, 255);
+		} else if (n == 5) {
+			FOREGROUND_COLOR = new Color(0, 255, 51);
+		} else if (n == 6) {
+			FOREGROUND_COLOR = new Color(255, 153, 0);
+		} else if (n == 7) {
+			FOREGROUND_COLOR = new Color(102, 0, 153);
+		} else if (n == 8) {
+			FOREGROUND_COLOR = new Color(255, 0, 0);
+		}
 
-        if (n >= 9) {
+		frame.add(new Shuffle_Game(n, 800, 50), BorderLayout.CENTER);
 
-            System.exit(1);
-        }
-        // center on the screen
-        frame.setLocationRelativeTo(null);
-        frame.setVisible(true);
+		frame.pack();
 
-    }
+		if (n >= 9) {
 
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> {
+			System.exit(1);
+		}
+		// center on the screen
+		frame.setLocationRelativeTo(null);
+		frame.setVisible(true);
 
-            nextGame();
+	}
 
-        });
-    }
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(() -> {
+
+			nextGame();
+		});
+	}
 }
